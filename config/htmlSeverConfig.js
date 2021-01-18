acf5b504bfdf97ec797e4401a79fbd6e0ff8a4c3
NeonGarden <mailyzhao@qq.com>
2021-01-18 15:18:07 +0800

    修改

--- config/htmlSeverConfig.js
+++ config/htmlSeverConfig.js
@@ -2,11 +2,10 @@ let htmlurl_config = ""
 
 if(process.env.NODE_ENV === 'development'){
     // 开发环境
-    // url_config = 'http://192.168.0.108:8085'
-	htmlurl_config = 'https://api.yohooshop.com'
+	htmlurl_config = 'http://localhost:8085'
 }else{
     // 生产环境
-    htmlurl_config = 'http://m.yohooshop.com'
+    htmlurl_config = 'http://localhost:8085'
 }
 
 export default htmlurl_config
